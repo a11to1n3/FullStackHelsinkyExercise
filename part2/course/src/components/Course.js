d3b274cfd7665a7e0ed39a2c10d9d3d9e83c3983
duyanhpham@outlook.com <duyanhpham@outlook.com>
2019-12-25 17:25:12 +0100

    Added course key for each course  Course exercise of part2

--- part2/course/src/components/Course.js
+++ part2/course/src/components/Course.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Courses = ({ courses }) => {
-  return courses.map(course => <Course course={course} />);
+  return courses.map(course => <Course key = {course.id} course={course} />);
 };
 
 const Course = ({ course }) => {
