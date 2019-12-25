de3f85ae4d20ef27e4dd83fa64a1d4ea3b2460ea
duyanhpham@outlook.com <duyanhpham@outlook.com>
2019-12-25 17:21:48 +0100

    Submitted Course exercise of part2

---
+++ part2/course/src/components/Course.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Courses = ({ courses }) => {
+  return courses.map(course => <Course course={course} />);
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <p style={{ fontWeight: "bold" }}>
+        total of {course.parts.reduce((sum, ex) => sum + ex.exercises, 0)}{" "}
+        exercises
+      </p>
+    </div>
+  );
+};
+
+const Header = ({ name }) => {
+  return <h1>{name}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return parts.map(part => (
+    <Part key={part.id} name={part.name} exercises={part.exercises} />
+  ));
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+export default Courses;
